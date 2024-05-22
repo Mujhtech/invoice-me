650c7a0e9f459a3b6b32c2945275cc7001eed8af
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2024-05-22 15:16:00 +0100

    chore: Handle error in editor and preview views

--- src/app/editor/[cid]/_view.tsx
+++ src/app/editor/[cid]/_view.tsx
@@ -3,18 +3,25 @@
 import React from "react";
 import { trpc } from "@/lib/server/api/client";
 import Editor from "../_components/editor";
+import { notFound } from "next/navigation";
 
 export default function View({ cid }: { cid: string }) {
-  const { data, isLoading, isRefetching } = trpc.invoice.get.useQuery({ cid });
+  const { data, isLoading, isRefetching, isError } = trpc.invoice.get.useQuery({
+    cid,
+  });
 
   if (isLoading || isRefetching) {
     return (
       <main className="h-full max-h-full overflow-hidden">
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col text-center items-center justify-center">
           Loading...
         </div>
       </main>
     );
+  }
+
+  if (isError) {
+    return notFound();
   }
 
   return <Editor data={data} editorMode={true} />;
